7e634ff30a3f68260d9aea5a7fb0220110f51aa4
czw0818 <75878576+czw0818@users.noreply.github.com>
2022-02-19 16:03:29 +0800

    builtin-function-update
    input ( ) -> String//read the stdin and return
    to_int (String) ->  int//parse string and return int
    to_num(String) ->num//parse string and return num

--- src/builtin/funcs.rs
+++ src/builtin/funcs.rs
@@ -43,14 +43,66 @@ impl BuiltInFuncs {
             }),
         ))
     }
+    fn func_input() -> RefCount<GloomFunc>{
+        RefCount::new(GloomFunc::new_builtin_fn(
+            Rc::new(String::from("input")),
+            Vec::with_capacity(0),
+            ReturnType::Have(DataType::Ref(RefType::String)),
+            false,
+            Rc::new(|_, _| {
+                let mut buf = String::new();
+                std::io::stdin().read_line(&mut buf).unwrap();
+                Value::Ref(GloomString::new(buf))
+            }),
+        ))
+    }
+    fn func_to_int(empty: Rc<String>) -> RefCount<GloomFunc>{
+        let params = vec![Param::new(empty, DataType::Ref(RefType::String))];
+        RefCount::new(GloomFunc::new_builtin_fn(
+            Rc::new(String::from("to_int")),
+            params,
+            ReturnType::Have(DataType::Int),
+            false,
+            Rc::new(|_,mut args|{
+                let arg  = args.vec.pop().unwrap();// arg:Value::Ref(GloomString(RefCell))
+                let rstr = arg.as_ref().downcast::<GloomString>().0.borrow();
+                let rint = rstr.parse().unwrap();
+                Value::Int(rint)
+            })),
+        )
+    }
+    fn func_to_num(empty: Rc<String>) -> RefCount<GloomFunc>{
+        let params = vec![Param::new(empty, DataType::Ref(RefType::String))];
+        RefCount::new(GloomFunc::new_builtin_fn(
+            Rc::new(String::from("to_num")),
+            params,
+            ReturnType::Have(DataType::Int),
+            false,
+            Rc::new(|_,mut args|{
+                let arg  = args.vec.pop().unwrap();
+                let rstr = arg.as_ref().downcast::<GloomString>().0.borrow();
+                let rnum = rstr.parse().unwrap();
+                Value::Num(rnum)
+            })),
+        )
+    }
     pub fn func_list() -> Vec<RefCount<GloomFunc>> {
         let empty_name = Rc::new(String::from(""));
-        vec![Self::func_println(empty_name.clone()),Self::func_print(empty_name)]
+        vec![
+        Self::func_println(empty_name.clone()),
+        Self::func_print(empty_name.clone()),
+        Self::func_to_int(empty_name.clone()),
+        Self::func_to_num(empty_name.clone()),
+        Self::func_input()
+        ]
     }
     pub fn func_map() -> HashMap<String, (u16, IsBuiltIn, IsPub, u16)> {
         let mut map = HashMap::new();
         map.insert(String::from("println"), (0, true, true, 0));
         map.insert(String::from("print"), (0, true, true, 0));
+        map.insert(String::from("input"  ), (0, true, true, 0));
+        map.insert(String::from("to_int" ), (0, true, true, 0));
+        map.insert(String::from("to_num" ), (0, true, true, 0));
         map
     }
 }
